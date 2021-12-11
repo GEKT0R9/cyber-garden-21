5cd130264a1c5e6beeb7254b7f059325be6ff040
GEKT0R9 <krupkin.vladimir@yandex.ru>
2021-12-11 16:03:30 +0400

    conf+

--- config/db.php
+++ config/db.php
@@ -7,7 +7,9 @@ $DATABASE_URL = [
     'pass' => '',
 ];
 
+$url = 'postgres://utccrldlyhnyst:eaf9327206de3ff5d8cb3572c0ada24cd20e60fd5b12a672c884d6220ccb45ab@ec2-54-155-87-214.eu-west-1.compute.amazonaws.com:5432/d5k65p2iq0t0o8';
 $DATABASE_URL = parse_url(getenv("DATABASE_URL"));
+$DATABASE_URL = parse_url($url);
 
 return [
     'class' => 'yii\db\Connection',
