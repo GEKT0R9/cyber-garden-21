e35d47f83ec952f131b619af6a74597f24177bc2
GEKT0R9 <krupkin.vladimir@yandex.ru>
2021-12-11 14:30:48 +0400

    h-memory error

--- config/web.php
+++ config/web.php
@@ -24,7 +24,7 @@ $config = [
             'enableAutoLogin' => true,
         ],
         'errorHandler' => [
-            'errorAction' => 'site/error',
+            'errorAction' => 'main/error',
         ],
         // 'mailer' => [
         //     'class' => 'yii\swiftmailer\Mailer',
